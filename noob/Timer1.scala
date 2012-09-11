7321a887718c5dfedac238df7e1d81238938643a
Eugene <yeo.eugene.oey@gmail.com>
2012-09-12 00:44:07 +0800

    Scala:'Hello World'

---
+++ noob/Timer1.scala
@@ -0,0 +1,15 @@
+object Timer
+{
+ def rpm(): Unit=
+ {
+  while (true)
+  {
+   System.out.println("Hello World")
+   Thread.sleep(1000)
+  }
+ }
+ def main(args: Array[String]): Unit=
+ {
+  rpm()
+ }
+}
---
+++ noob/Timer3.scala
@@ -0,0 +1,15 @@
+object Timer
+{
+ def rpm(callback: () => Unit): Unit=
+ {
+  while (true)
+  {
+   callback()
+   Thread.sleep(1000)
+  }
+ }
+ def main(args: Array[String]): Unit=
+ {
+  rpm(()=>Console.println("Hello World"))
+ }
+}
